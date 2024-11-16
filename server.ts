e55af9379466fc047680174db860311ee83d0a1a
Nuwan Fernando <nuwaninfo@gmail.com>
2024-11-16 08:50:15 +0200

    changeed the port number

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import express, {Express} from "express"
 
 const app: Express = express()
-const port: number = 8000
+const port: number = 3000
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
